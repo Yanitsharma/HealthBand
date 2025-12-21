b2e6b37e59270acb406de401680869b0eec2ddaf
Yanitsharma <yanit.sharma.mec22@itbhu.ac.in>
2025-12-21 09:58:35 +0530

    changed

--- hclbackend/server.js
+++ hclbackend/server.js
@@ -30,7 +30,8 @@ const io = new Server(server, {
     origin: [
         "http://localhost:5173", 
         "http://localhost:5174",
-        "https://healthband-2.onrender.com" // Update this with your ACTUAL Render URL
+        "https://healthband-2.onrender.com" ,// Update this with your ACTUAL Render URL
+        "https://healthbband.vercel.app/"
     ],
     methods: ["GET", "POST"],
   },
