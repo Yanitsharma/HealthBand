e6cf797612f2ec016846426551a8dfa928e37ba7
Yanitsharma <yanit.sharma.mec22@itbhu.ac.in>
2025-12-21 10:12:46 +0530

    Stop tracking node_modules

--- hclfrontend/src/utils/api.js
+++ hclfrontend/src/utils/api.js
@@ -1,5 +1,5 @@
 // API Base URL
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://healthband-3.onrender.com/api';
 
 // Helper function to get auth headers
 const getAuthHeaders = () => {
